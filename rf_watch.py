85fd5baac8f4e2612373e2af8dbeaaace2ba6c10
root <root@raspberrypi.(none)>
2014-05-12 03:32:02 +0000

    First rewrites of watchdog

--- rf_watch.py
+++ rf_watch.py
@@ -4,3 +4,18 @@
 #   by Connor Nishijima and Andrew Neal
 #
 #/////////////////////////////////////////////
+
+directory = "/home/pi/wac2/"
+
+def commandCheck():
+	f = open(directory + "command","r")
+	command = f.read().strip("\n")
+	f.close
+	if not command == "":
+		return command
+		print "RECEIVED COMMAND: " + str(command)
+	else:
+		return null
+
+while True:
+	commandCheck()
